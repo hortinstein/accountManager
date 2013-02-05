e2b4d5c1a99c3114fce2927c88e731d61554bda9
hortinstein <hortinstein@hortinstein-VirtualBox.(none)>
2013-02-05 23:19:23 +0900

    started adding riak support, still errors

--- databaseMiddleware/riak/riakTheSlit.js
+++ databaseMiddleware/riak/riakTheSlit.js
@@ -1,110 +1,108 @@
 var utility = require("./utility.js"); //store for functions not in the standard template
-var riakClient = require('riak');
-
+var riak = require('riak');
+var request = require('request');
 var DB = {}; //object to export that will contain all of the templated functions 
-
-var userpass= '';
+var userpass = '';
 var username = '';
-var	database_name = '';
+var bucket_name = '';
+var riakClient = '';
 
-DB.setup = function  (config) {
-	var servers = [config.riak_host];
+DB.setup = function(config) {
+	var riak_host_string = [config.riak_host + ':'+ config.riak_port];
+	console.log(riak_host_string);
 	var client_id = "acctMgr-client";
-	var pool_name = "accts-pool"
-	var bucket_name = config.database_name
-	var client = new riakClient(servers, client_id, pool_name);
+	var pool_name = "acctMgr-pool";
+	bucket_name = config.database_name;
+	riakClient = new riak(riak_host_string, client_id, pool_name);
+	riakClient.debug_mode = false;
 }
 module.exports = DB;
 
 
-
-DB.update = function  (newData,callback) {
-	// var multi = redisClient.multi();
-	// var recordKey = database_name+":"+newData.username;
-	// redisClient.exists(recordKey, function(e,r) {
-	// 	if (r){
-	// 		multi.set(database_name+":"+newData.email, newData.username);
-	// 		for (var attrname in newData) { 
-	// 		    multi.hset(database_name+":"+newData.username, attrname,newData[attrname]);
-	// 		}
-	// 		multi.exec();
-	// 		callback(null,'ok');
-
-	// 	} else {
-	// 		callback('user_not_found');
-	// 	}
-	// });
+DB.update = function(newData, callback) {
+	//console.log(newData);
+	riakClient.get(bucket_name, newData.username, function(e, r, o) {
+		if(r) {
+			var options = {
+				http_headers: {
+					'x-riak-index-username_bin': username,
+					'x-riak-index-username_email_bin': email
+				}
+			}
+			riakClient.put(bucket_name, newData.username, newData, function(e, r, o) {
+				if(e) {
+					callback(e);
+				} else {
+					callback(null, 'ok');
+				}
+			});
+		} else {
+			callback('user_not_found');
+		}
+	});
 }
 
 
 
-DB.insert = function  (record,callback) {
-	// var multi = redisClient.multi();
-	// var recordKey = database_name+":"+record.username;
-	// redisClient.exists(recordKey, function(e,r) {
-	// 	if (r){
-	// 		callback('record_exists');
+DB.insert = function(record, callback) {
+	riakClient.get(bucket_name, record.username, function(e, r, o) {
+		console.log(e,r,o);
+		if(r) {
+			callback('record_exists');
+		} else {
+			DB.update(record,callback);
+		}
 
-	// 	} else {
-	// 		multi.set(database_name+":"+record.email, record.username);
-	// 		for (var attrname in record) { 
-	// 		    multi.hset(database_name+":"+record.username, attrname,record[attrname]);
-	// 		}
-	// 		multi.exec();
-	// 		callback(null,'ok');
-	// 	}
-	// });
+	});
 }
 
 //getByUsername
 //in: username, callback 
-DB.getByUsername = function  (username, callback) {
-	// var client = DB.nano.use(database_name); //sets it to the right database
-// 	var recordKey = database_name+":"+username;
-// 	redisClient.hgetall(recordKey,function(e,r) {
-// 		if(r === null){
-// 			callback('user_not_found')
-// 		}
-// 		else{
-// 			callback(null,r);
-// 		}	
-// 	});
+DB.getByUsername = function(username, callback) {
+	riakClient.get(bucket_name, username, function(e, r, o) {
+		if (e) {
+			callback('user_not_found');
+		} else {
+			callback(null,o);
+		}
+	});
 }
 
 //getByEmail
 //in: username, callback 
-DB.getByEmail = function  (email, callback) {
-// var client = DB.nano.use(database_name); //sets it to the right database
-	// var recordKey = database_name+":"+email;
-	// redisClient.get(recordKey,function(e,r) {
-	// 	recordKey = database_name+":"+r;
-	// 	if(!r){
+DB.getByEmail = function(email, callback) {
+	// var client = DB.nano.use(database_name); //sets it to the right database
+	// client.view('userAccount','email', {key: email}, function(e, r){ //checks to see if email is already registered
+	// 	if (r === null || r === undefined)
+	// 	{
+	// 		callback('user_not_found', null);
+	// 	}
+	// 	else if((r.rows).length === 0) //email already exists
+	// 	{
 	// 		callback('user_not_found',null);
 	// 	}
-	// 	else{
-	// 		redisClient.hgetall(recordKey,function(e,r) {
-	// 			if(e){
-	// 				callback('user_not_found',null);
-	// 			}
-	// 			else{
-	// 				callback(null,r);
-	// 			}	
-	// 		});
-	// 	}	
+	// 	else //email already exists
+	// 	{
+	// 		var id = r.rows[0].id;
+	// 		DB.getByUsername(id,callback);
+	// 	};
 	// });
 }
 
 //sets up the Db 
 DB.buildDB = function(callback) {
-	// callback(); //no bookeeping needed here
+	callback();
 }
 //destroy the DB created by accountManager
 DB.destroyDB = function(callback) { //because some men just want to watch the world burn
-	// redisClient.keys(database_name+"*",function (err, replies) {
-	//     replies.forEach(function (reply, index) {
- //        	redisClient.del(reply.toString());
- //        });
- //        callback();
- //    }); // removing all keys
- 
+	request.get({
+		url: 'http://' + riak_host + '/buckets/' + bucket + '/keys?keys=true',
+		json: true
+	}, function(e, r, b) {
+		for(key in b.keys) {
+			client.del(bucket, b.keys[key], function() {
+			});
+		};
+		callback();
+	})
 };
